775b6da315b24584a1f1fcdeb16bb76622a2f225
Artur Nista <arturnista@gmail.com>
2017-11-14 17:29:57 -0200

    build

--- src/App.js
+++ src/App.js
@@ -8,18 +8,21 @@ class App extends Component {
             <header className="App-header">
                 <img src='/images/moharu.jpg' className="App-logo" alt="logo" />
                 <p className="App-title-text">Bem vindo ao</p>
-                <h1 className="App-title">MOHARU NEWS</h1>
+                <a href='https://www.facebook.com/moharuow' className='facebook-link'>
+                    <h1 className="App-title">MOHARU NEWS</h1>
+                </a>
+                <p className="App-title-text">O portal de informação do seu jogador favorito</p>
             </header>
             <div className='news-container'>
                 <h2 className="news-title">Moharu para a divisão Brasileira de Overwatch do Brasil de Pelotas</h2>
                 <p className='news-text'>
-                    O MOHARU NEWS ficou sabendo com exclusividade, através de uma fonte confiável conhecida como 'LIBERACU', sobre a contratação de João "Moharu" Teló para a divisão brasileira de Overwatch do time gaúcho Brasil de Pelotas.
+                    O MOHARU NEWS ficou sabendo com exclusividade, através de uma fonte confiável conhecida como 'LIBERACU', sobre a contratação de <span className='moharu-name'>João "Moharu" Teló</span> para a divisão brasileira de Overwatch do time gaúcho Brasil de Pelotas.
                 </p>
                 <p className='news-text'>
                     Além do time de Overwatch, a organização também possui grandes equipes de futebol, bocha, bolão e rouba monte.
                 </p>
                 <p className='news-text'>
-                    Moharu deve começar sua atuação na equipe à partir de Janeiro de 2018, após finalizar os campeonatos com sua equipe atual, Zony Gaming.
+                    <span className='moharu-name'>Moharu</span> deve começar sua atuação na equipe à partir de Janeiro de 2018, após finalizar os campeonatos com sua equipe atual, Zony Gaming.
                 </p>
             </div>
             <div className='news-container'>
@@ -28,7 +31,7 @@ class App extends Component {
                     O MOHARU NEWS ficou sabendo com exclusividade através do próprio player que o seu time deverá substituir um time qualquer no campeonato brasileiro de Overwatch.
                 </p>
                 <p className='news-text'>
-                    Nas palavras do jogador Moharu, "O campeonato vai ser ezi d+ irmãozinho"
+                    Nas palavras do jogador <span className='moharu-name'>Moharu</span>, "O campeonato vai ser ezi d+ irmãozinho"
                 </p>
                 <p className='news-text'>
                     10min pra acabar o expediente
